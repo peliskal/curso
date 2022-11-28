41f50058c7ced83e349b2457be0fe0201319c9bf
peliskal <109911166+peliskal@users.noreply.github.com>
2022-11-27 23:41:27 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
  
  function sumar(){
  	var suma = new Array();
- 	suma[0] = document.getElementById('mnns1').value;
- 	suma[1] = document.getElementById('mnns2').value;
+ 	suma[0] = document.getElementById('ns1').value;
+ 	suma[1] = document.getElementById('ns2').value;
  	return alert((suma[0]+suma[1]));
  }
 
